afa1e73705f2d44b8030259eb0c3ffa0a4892676
TheBialy3 <karbialasol@gmail.com>
2023-06-27 21:38:55 +0200

    css added

--- src/main/java/com/bialas/lif/thymeleaf/ThymeleafApplication.java
+++ src/main/java/com/bialas/lif/thymeleaf/ThymeleafApplication.java
@@ -2,6 +2,7 @@ package com.bialas.lif.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 public class ThymeleafApplication {
@@ -9,5 +10,12 @@ public class ThymeleafApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThymeleafApplication.class, args);
     }
+
+
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        if (!registry.hasMappingForPattern("/assets/**")) {
+//            registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/css/");
+//        }
+//    }
 
 }
