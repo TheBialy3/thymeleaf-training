78a2be9021ae1fde1c1a8e3fc192317750068ecd
TheBialy3 <karbialasol@gmail.com>
2023-07-01 22:37:13 +0200

    css added some look

--- src/main/java/com/bialas/lif/thymeleaf/CarControllre.java
+++ src/main/java/com/bialas/lif/thymeleaf/CarControllre.java
@@ -14,12 +14,55 @@ public class CarControllre {
         cars.add(new Car("BMW", "i8"));
         cars.add(new Car("Fiat", "126p"));
     }
-    @GetMapping("/car")
-    public String get(Model model) {
+    @GetMapping("/main")
+    public String getMain(Model model) {
         setCars();
         model.addAttribute("name", "karol");
         model.addAttribute("cars", cars);
 
         return "main";
+    }
+
+    @GetMapping("/projects")
+    public String getProjects(Model model) {
+        model.addAttribute("name", "karol");
+        return "projects";
+    }
+
+    @GetMapping("/ideas")
+    public String getIdeas(Model model) {
+        model.addAttribute("name", "karol");
+
+        return "projectTemplate/ideas";
+    }
+    @GetMapping("/java")
+    public String getJava(Model model) {
+        model.addAttribute("name", "karol");
+
+        return "projectTemplate/java";
+    }
+    @GetMapping("/unity")
+    public String getUnity(Model model) {
+        model.addAttribute("name", "karol");
+
+        return "projectTemplate/unity";
+    }
+    @GetMapping("/about")
+    public String getAbout(Model model) {
+        model.addAttribute("name", "karol");
+
+        return "about";
+    }
+    @GetMapping("/contactMe")
+    public String getContactMe(Model model) {
+        model.addAttribute("name", "karol");
+
+        return "contactMe";
+    }
+    @GetMapping("/portfolio")
+    public String getPortfolio(Model model) {
+        model.addAttribute("name", "karol");
+
+        return "projectTemplate/portfolio";
     }
 }
